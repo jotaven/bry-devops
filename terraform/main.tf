35557f39881463ab8385f867499cee6b25c7b1c2
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2025-11-09 00:11:57 -0300

    fix(eks): Habilita um endpoint público para o eks ser gerenciado de fora

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,9 @@ module "eks" {
   cluster_name    = "jotinha-eks-cluster"
   cluster_version = "1.29"
 
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnet_ids
 
